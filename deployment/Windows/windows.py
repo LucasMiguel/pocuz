8a07d69504c45887ec6184d5c08bd933d61ccc22
LucasMiguel <lucasoliveira129@hotmail.com>
2022-02-14 22:19:34 -0300

    Finalizado

---
+++ deployment/Windows/windows.py
@@ -0,0 +1,28 @@
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    '../src/main.py',
+    '--windowed',
+    '--icon=../images/icon.ico',
+    # Incluindo o banco de dados
+    '--add-data=../data/data.json;data',
+    # Incluindo arquivos de imagem
+    '--add-binary=../images/break.png;images',
+    '--add-binary=../images/concentration.png;images',
+    '--add-binary=../images/icon_32_break.png;images',
+    '--add-binary=../images/icon_32.png;images',
+    '--add-binary=../images/icon_about.png;images',
+    '--add-binary=../images/icon.ico;images',
+    '--add-binary=../images/icon.png;images',
+    '--add-binary=../images/menu_button.png;images',
+    '--add-binary=../images/pause.png;images',
+    '--add-binary=../images/play.png;images',
+    '--add-binary=../images/undo.png;images',
+    # Incluindo o som da notificação
+    '--add-binary=../sound/notification_sound.wav;sound',
+    # Inlcuindo a fonte
+    '--add-binary=../fonts/Lato-Light.ttf;fonts',
+    # Incluindo as depedências
+    '--paths=../env/lib/python3.10/site-packages/',
+    '--name=pocuz'
+])
--- deployment/linux.py
+++
@@ -1,7 +0,0 @@
-import PyInstaller.__main__
-
-PyInstaller.__main__.run([
-    '../src/main.py',
-    '--paths=../env/lib/python3.10/site-packages/',    
-    '--name=pocuz'
-])
